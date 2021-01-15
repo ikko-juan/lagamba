cabb43025a9dd1c9d01047a55538c0ef55a88f68
danielhergil <daniel.hergil@gmail.com>
2021-01-15 18:57:52 +0100

    feat: add ground identifier
    Add some lines in the PlayerController script that helps to identify when the player is in the ground layer or on the air to avoid a second jump on the air

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,19 +9,33 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
 
     private Rigidbody2D myRigidbody;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D mycollider;
+
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        mycollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        grounded = Physics2D.IsTouchingLayers(mycollider, whatIsGround);
+
         myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
 
         if(Input.GetKeyDown(KeyCode.Space)) {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            if(grounded)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
+            
         }
     }
 }
